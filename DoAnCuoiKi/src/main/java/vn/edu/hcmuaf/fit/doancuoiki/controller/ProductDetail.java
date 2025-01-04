535e036d3b425a7a5acf103b9931cc5a287d90a7
NguyenNguyen-22 <22130189@st.hcmuaf.edu.vn>
2025-01-04 10:55:31 +0700

    update ProductDetail

--- DoAnCuoiKi/src/main/java/vn/edu/hcmuaf/fit/doancuoiki/controller/ProductDetail.java
+++ DoAnCuoiKi/src/main/java/vn/edu/hcmuaf/fit/doancuoiki/controller/ProductDetail.java
@@ -3,14 +3,24 @@ package vn.edu.hcmuaf.fit.doancuoiki.controller;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import vn.edu.hcmuaf.fit.doancuoiki.dao.ProductDao;
+import vn.edu.hcmuaf.fit.doancuoiki.model.Product;
 
 import java.io.IOException;
 
-@WebServlet(name = "ProductDetail", value = "/ProductDetail")
+@WebServlet(name = "ProductDetail", value = "/product")
 public class ProductDetail extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int pid= Integer.parseInt(request.getParameter("pid"));
 
+        // Lấy chi tiết sản phẩm từ DB
+        ProductDao productDao = new ProductDao();
+        Product product = productDao.getProductById(pid);
+
+        // Gửi sản phẩm vào trang JSP
+        request.setAttribute("p", product);
+        request.getRequestDispatcher("product-detail.jsp").forward(request, response);
     }
 
     @Override
