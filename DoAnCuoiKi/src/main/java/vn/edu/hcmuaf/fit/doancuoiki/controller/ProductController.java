ceb8f755d712f3787275d48314f52b8293a62645
Mai Lam Nhat <22130195@st.hcmuaf.edu.vn>
2025-01-13 22:20:37 +0700

    làm tiếp giỏ hàng

--- DoAnCuoiKi/src/main/java/vn/edu/hcmuaf/fit/doancuoiki/controller/ProductController.java
+++ DoAnCuoiKi/src/main/java/vn/edu/hcmuaf/fit/doancuoiki/controller/ProductController.java
@@ -1,15 +1,24 @@
 package vn.edu.hcmuaf.fit.doancuoiki.controller;
 
+import vn.edu.hcmuaf.fit.doancuoiki.model.Product;
+import vn.edu.hcmuaf.fit.doancuoiki.service.IProductService;
+import vn.edu.hcmuaf.fit.doancuoiki.service.ProductService;
+
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ProductController", value = "/ProductController")
 public class ProductController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        IProductService productService = new ProductService();
+        List<Product> products = productService.findAll();
+        request.setAttribute("products",products);
+        String url = "/productsList.jsp";
+        request.getRequestDispatcher(url).forward(request,response);
 
     }
 
